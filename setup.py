8f81eb0c06b12e1e497dff988931007f54a3bb1d
rudolf <rudolf.degeijter@meemoo.be>
2022-05-03 10:19:10 +0200

    Add setup.py for packaging. Also added a __init__ so the module can be found by setup.py.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mediahaven",
+    version="0.1.1",
+    license="GPL",
+    author="Mattias",
+    author_email="mattias.poppe@meemoo.be",
+    packages=find_packages(exclude=["tests"]),
+    long_description=open("README.md", encoding="utf8").read(),
+    zip_safe=False,
+    setup_requires=["wheel"],
+    install_requires=[
+        "oauthlib==3.1.0",
+        "requests_oauthlib==1.3.0",
+        "requests"
+    ],
+)
