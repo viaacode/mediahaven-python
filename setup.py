40cd78149b95ffe965bfdac620813f3bfefa33f5
Mattias.Poppe <poppe.mattias@gmail.com>
2022-09-20 12:26:57 +0200

    Bump version to v0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="mediahaven",
-    version="0.1.1",
+    version="0.1.2",
     license="GPL",
     author="Mattias",
     author_email="mattias.poppe@meemoo.be",
@@ -10,9 +10,5 @@ setup(
     long_description=open("README.md", encoding="utf8").read(),
     zip_safe=False,
     setup_requires=["wheel"],
-    install_requires=[
-        "oauthlib==3.1.0",
-        "requests_oauthlib==1.3.0",
-        "requests"
-    ],
+    install_requires=["oauthlib==3.1.0", "requests_oauthlib==1.3.0", "requests"],
 )
