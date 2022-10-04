6f7663a5b661126896571200c21d39db52ddaf16
lennertvandevelde <lennert.van.de.velde@outlook.be>
2022-10-04 16:32:39 +0200

    MHPY-8 Adds organisations to init

--- mediahaven/__init__.py
+++ mediahaven/__init__.py
@@ -5,9 +5,10 @@ from mediahaven.mediahaven import MediaHavenClient
 
 # Records
 from mediahaven.resources.records import Records
-
+from mediahaven.resources.organisations import Organisations
 
 class MediaHaven(MediaHavenClient):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.records = Records(self)
+        self.organisations = Organisations(self)
