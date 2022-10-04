cab248234da88143d43a9630fb0b533b36744747
lennertvandevelde <lennert.van.de.velde@outlook.be>
2022-10-04 16:36:29 +0200

    Merge conflict: back to main

--- mediahaven/__init__.py
+++ mediahaven/__init__.py
@@ -6,12 +6,10 @@ from mediahaven.mediahaven import MediaHavenClient
 # Records
 from mediahaven.resources.records import Records
 from mediahaven.resources.field_definitions import FieldDefinitions
-from mediahaven.resources.organisations import Organisations
 
 
 class MediaHaven(MediaHavenClient):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.records = Records(self)
-        self.organisations = Organisations(self)
         self.fields = FieldDefinitions(self)
