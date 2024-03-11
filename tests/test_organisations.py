a38717ee5d8a0e153edd30ac9c0c20009c30ba6e
lennertvandevelde <lennert.van.de.velde@outlook.be>
2024-03-11 14:41:33 +0100

    MHPY-22 Test for org by externalid

--- tests/test_organisations.py
+++ tests/test_organisations.py
@@ -26,6 +26,23 @@ class TestOrganisations:
             mh_client_mock._get(), AcceptFormat.JSON
         )
     
+    @patch("mediahaven.resources.organisations.MediaHavenSingleObjectCreator")
+    def test_get_by_external_id(self, object_creator_mock, organisations: Organisations):
+        # Arrange
+        external_id = "OR-a11111a"
+        mh_client_mock = organisations.mh_client
+
+        # Act
+        organisations.get_by_external_id(external_id)
+
+        # Assert
+        mh_client_mock._get.assert_called_once_with(
+            f"{organisations.name}/ExternalId:{external_id}", AcceptFormat.JSON
+        )
+        object_creator_mock.create_object.assert_called_once_with(
+            mh_client_mock._get(), AcceptFormat.JSON
+        )
+    
     @patch("mediahaven.resources.organisations.MediaHavenPageObjectCreator")
     def test_search(self, object_creator_mock, organisations: Organisations):
         # Arrange
